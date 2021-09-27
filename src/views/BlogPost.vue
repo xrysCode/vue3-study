53746a24c4a25ef7a72662f1151d1644d1125568
xrysCode <xrysemail@163.com>
2021-09-27 21:51:38 +0800

    1

---
+++ src/views/BlogPost.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="about">
+    <h1>有了render（）  这个还会展示吗？ 这个优先级高于render（）?</h1>
+  </div>
+</template>
+<script>
+// <template>
+//   <div class="about">
+//     <h1>有了render（）  这个还会展示吗？</h1>
+//   </div>
+// </template>  这个优先级高于render（）
+import { h } from 'vue'
+export default {
+  auth: true,
+  render () {
+    debugger
+    if (this.$.type.auth) {
+      //       const ButtonCounter = resolveComponent('ButtonCounter')
+    //   return h(ButtonCounter)
+      return h('div', [
+        h('header', this.$slots.header()),
+        h('main', this.$slots.default()),
+        h('footer', this.$slots.footer())
+      ])
+    } else {
+      return h('div', null, null)
+    }
+  }
+}
+</script>
---
+++ src/views/User.vue
@@ -0,0 +1,7 @@
+<template>
+<!-- 路径参数 用冒号 : 表示。当一个路由被匹配时，它的 params 的值将在每个组件中以 this.$route.params 的形式暴露出来。 -->
+  <div class="user">
+      <h2>User {{ $route.params.id }}</h2>
+      <router-view></router-view>
+    </div>
+</template>
