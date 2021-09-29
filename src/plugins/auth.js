8270ba2a1445e1aea392c11612f857d216e264d7
xrysCode <90566708+xrysCode@users.noreply.github.com>
2021-09-29 13:39:09 +0800

    Update auth.js

--- src/plugins/auth.js
+++ src/plugins/auth.js
@@ -1,65 +1,136 @@
 /* eslint-disable no-unused-vars */
-/* eslint-disable no-unused-expressions */
 
-// 组件解析
-function ComponentTree (name, filePath, authority, routerPath, routerName) {
-  this.name = name
-  this.filePath = filePath
-  this.authority = authority
-  this.childNode = []
-  this.routerPath = routerPath
-  this.routerName = routerName
-}
-
-function findComponent (components, parentNode) {
-  if (components) {
-    const array = Object.values(components)
-    array.forEach(element => {
-      const node = new ComponentTree(element.name, element.__file, element.authority)
-      parentNode.childNode.push(node)
-      if (element.components) {
-        findComponent(element.components, node)
+/**
+ * 组件解析
+ *  node_modules\@vue\shared\dist\shared.d.ts 273
+    ELEMENT = 1,
+    FUNCTIONAL_COMPONENT = 2,
+    STATEFUL_COMPONENT = 4,
+    TEXT_CHILDREN = 8,
+    ARRAY_CHILDREN = 16,
+    SLOTS_CHILDREN = 32,
+    TELEPORT = 64,
+    SUSPENSE = 128,
+    COMPONENT_SHOULD_KEEP_ALIVE = 256,
+    COMPONENT_KEPT_ALIVE = 512,
+    COMPONENT = 6  这是一个有毒的数字
+ * @param {Array<VNode>} _vChildren 虚拟节点数组
+ * @param {*} parentNode 解析的父节点
+ * @returns componentNode
+ */
+function parseComponentTree (_vChildren, parentNode) {
+  for (let i = 0; i < _vChildren.length; i++) {
+    const _vNode = _vChildren[i]
+    if (_vNode.shapeFlag === 6 || _vNode.shapeFlag & (2 + 4 + 256 + 512)) { // 组件节点 寻找component
+      const _vComponent = _vNode.component
+      if (!_vComponent) {
+        console.warn('找到组件component为null', _vNode)
+        continue
       }
-    })
-  }
-}
-
-function fillTree (app, currentRoute) {
-  const rootComponent = app._component
-  const rootTree = new ComponentTree(rootComponent.name, rootComponent.__file, rootComponent.authority, currentRoute.path, currentRoute.name)
-  findComponent(app._component.components, rootTree)
-  return rootTree
-}
-
-// 路由解析
-function resolveRouterMap (routerConfigArr) {
-  const routeMap = new Map()
-  routerConfigArr.forEach(route => { // 路由中寻找组件
-    const comp = route.components.default
-    if (comp instanceof Function) {
-      console.log('Promise 如何拿到路由的懒加载', comp)
-      comp().then(comp => {
-        console.log('懒加载的路由s', comp)
-        const lazyComp = comp.default
-        const lazyCom = new ComponentTree(lazyComp.name, lazyComp.__file, lazyComp.authority, route.path, route.name)
-
-        findComponent(lazyComp.components, lazyCom)
-        routeMap.set(route.path, lazyCom)
-      })
-    } else {
-      const com = new ComponentTree(comp.name, comp.__file, comp.authority, route.path, route.name)
-
-      findComponent(comp.components, com)
-
-      routeMap.set(route.path, com)
+      const componentNode = ComponentNode(_vComponent, _vNode.shapeFlag, parentNode)
+      // ref 被用来给元素或子组件注册引用信息。引用信息将会被注册在父组件的 $refs 对象上。如果在普通的 DOM 元素上使用，引用指向的就是那个 DOM 元素；如果用在子组件上，引用就指向组件实
+      const vSubTree = _vComponent.subTree
+      if (vSubTree.children) {
+        parseComponentTree(_vComponent.subTree.children, componentNode)
+      }
+      if (vSubTree.ref) { // 有引用子组件 如果是路由的RouterView 那么_vComponent.subTree.ref.r._value.$==_vComponent.subTree.component==>true
+        const vSubComponent = vSubTree.component
+        const subComponentNode = ComponentNode(vSubComponent, vSubTree.shapeFlag, componentNode)
+        console.info('寻找到关联子树 父组件=', _vComponent, '子组件', vSubTree.component)
+        parseComponentTree(vSubComponent.subTree.children, subComponentNode)
+      }
+    } else { // 寻找普通标签节点里面的组件  找children
+      // v_node.children 有可能是文本节点
+      if (_vNode.shapeFlag & (16)) { // shapeFlag: 17 16
+        parseComponentTree(_vNode.children, parentNode)
+      }
     }
-  })
-  console.log(routeMap)
+  }
 
-  // 将路由解析结果挂到组件中
-  // var xx=app._context.components.RouterLink.props.to.type
-  return routeMap
+  // 组件节点   (componentNode.shapeFlag === 6 || componentNode.shapeFlag & (4 + 16 + 256 + 512))
 }
+const routerLinkMap = new Map()// k 路径，对应自己的to连接， v 自己的解析节点
+const routerViewMap = new Map()// k 当前路径，v RouterView的整个子项
+let rootComponentNode
+let vRouter
+function ComponentNode (_vComponent, shapeFlag, parentNode) {
+  // const componentNode = Object.create({ childNodes: [],parentNode })
+  const componentNode = { childNodes: [], parentNode }
+  if (parentNode != null) {
+    parentNode.childNodes.push(componentNode)
+  }
+  componentNode.name = _vComponent.type.name// 组件名
+  componentNode.filePath = _vComponent.type.__file// 组键路径
+  componentNode.shapeFlag = shapeFlag
+  // this.authority = vComponent.type.authority
+  switch (componentNode.name) {
+    case 'RouterLink':
+      componentNode.to = _vComponent.props.to// 路由连接
+      // componentNode.matchedPath="xxxxx"
+      componentNode.isRouter = true
+      routerLinkMap.set(componentNode.to, componentNode)
+      break
+    case 'RouterView':
+      componentNode.isRouter = true
+
+      // routerViewMap.set()
+      break
+    default :
+      componentNode.isRouter = false
+  }
+  // if (componentNode.isRouter) {
+  //   const vMatchedArr = vRouter.currentRoute.value.matched// 代表多个view组件
+  //   vMatchedArr.forEach(a => {
+
+  //   })
+  //   if (vMatchedArr.l) { vCurrentRoute.matched }
+  // }
+  return componentNode
+}
+
+// function findComponent (components, parentNode) {
+//   if (components) {
+//     const array = Object.values(components)
+//     array.forEach(element => {
+//       const node = new parseComponentTree(element.name, element.__file, element.authority)
+//       parentNode.childNode.push(node)
+//       if (element.components) {
+//         findComponent(element.components, node)
+//       }
+//     })
+//   }
+// }
+// function fillTree (app, currentRoute) {
+//   const rootComponent = app._component
+//   const rootTree = new parseComponentTree(rootComponent.name, rootComponent.__file, rootComponent.authority, currentRoute.path, currentRoute.name)
+//   findComponent(app._component.components, rootTree)
+//   return rootTree
+// }
+// 路由解析
+// function resolveRouterMap (routerConfigArr) {
+//   const routeMap = new Map()
+//   routerConfigArr.forEach(route => { // 路由中寻找组件
+//     const comp = route.components.default
+//     if (comp instanceof Function) {
+//       console.log('Promise 如何拿到路由的懒加载', comp)
+//       comp().then(comp => {
+//         console.log('懒加载的路由s', comp)
+//         const lazyComp = comp.default
+//         const lazyCom = new parseComponentTree(lazyComp.name, lazyComp.__file, lazyComp.authority, route.path, route.name)
+//         findComponent(lazyComp.components, lazyCom)
+//         routeMap.set(route.path, lazyCom)
+//       })
+//     } else {
+//       const com = new parseComponentTree(comp.name, comp.__file, comp.authority, route.path, route.name)
+//      findComponent(comp.components, com)
+//       routeMap.set(route.path, com)
+//     }
+//   })
+//   console.log(routeMap)
+//   // 将路由解析结果挂到组件中
+//   // var xx=app._context.components.RouterLink.props.to.type
+//   return routeMap
+// }
 
 export default {
   install: (app, options) => {
@@ -83,8 +154,28 @@ export default {
           // e.target 是你当前点击的元素
           // e.currentTarget 是你绑定事件的元素
           console.log('e.target=', e.target, 'e.currentTarget', e.currentTarget)
-          // const v = e.target.__vnode
-          // shapeFlag
+          const recentlyComponent = e.target.__vueParentComponent// 最近的一个组件
+          const rootComponet = recentlyComponent.root
+          if (!rootComponentNode) { // 初始化整个树
+            vRouter = recentlyComponent.appContext.config.globalProperties.$router
+            rootComponentNode = ComponentNode(rootComponet, -1, null)
+            parseComponentTree(rootComponet.subTree.children, rootComponentNode)
+          }
+          // 向上查找父级，直到遇到路由RouterView为止
+          let fragmentUpComponet = recentlyComponent
+          while (fragmentUpComponet) {
+            if (fragmentUpComponet.type.name === 'RouterView') {
+              break
+            }
+            fragmentUpComponet = fragmentUpComponet.__vueParentComponent
+          }
+          // 比较查看是否是和根树相同，如果相同就结束
+          if (fragmentUpComponet === rootComponet) {
+            return
+          }
+
+          console.log('组件树', rootComponentNode)
+
           // e.target.__vueParentComponent
           // console.log('---', e, document.getSelection())
         }, false)
