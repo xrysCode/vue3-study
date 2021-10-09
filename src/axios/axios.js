0137c99d96069c09b83d1a4b87bb993cee9b5ca5
xrysCode <90566708+xrysCode@users.noreply.github.com>
2021-10-09 18:10:12 +0800

    Update axios.js

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -3,23 +3,38 @@ import axios from 'axios'
 
 // Set config defaults when creating the instance
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:9000/'
+  baseURL: 'http://47.112.148.138:9000/'
 })
+
+const f = () => {
+  return Math.random() + ''
+}
+const q = f()
 // Alter defaults after instance has been created
-// instance.defaults.headers.common.Authorization = AUTH_TOKEN
+instance.defaults.headers.common.Authorization = q// 'xxx'
+
+// AUTH_TOKEN
 // instance.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 // 覆写库的超时默认值
 // 现在，在超时前，所有请求都会等待 2.5 秒
 // instance.defaults.timeout = 2500;
 
 // 添加请求拦截器
-// instance.interceptors.request.use(function (config) {
-//   // 在发送请求之前做些什么
-//   return config
-// }, function (error) {
-//   // 对请求错误做些什么
-//   return Promise.reject(error)
-// })
+instance.interceptors.request.use(function (config) {
+  // 在发送请求之前做些什么
+  debugger
+  const a = config.data.data
+  const b = a.dataRule
+  delete a.dataRule
+  config.data.data = a
+  console.log(a, b)
+  config.headers.common.auth = 'zzzz'
+
+  return config
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error)
+})
 
 // // 添加响应拦截器
 // instance.interceptors.response.use(function (response) {
